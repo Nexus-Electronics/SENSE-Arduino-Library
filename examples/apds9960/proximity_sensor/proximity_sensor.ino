0d124afce45c0000d5cc90591c7013f28a6ce96a
ZBoss123 <63132143+ZBoss123@users.noreply.github.com>
2022-09-20 19:36:26 -0700

    Add files via upload

---
+++ examples/apds9960/proximity_sensor/proximity_sensor.ino
@@ -0,0 +1,43 @@
+/***************************************************************************
+  This is a library for the APDS9960 digital proximity, ambient light, RGB, and gesture sensor
+
+  This sketch puts the sensor in proximity mode and enables the interrupt
+  to fire when proximity goes over a set value
+
+  Designed specifically to work with the Adafruit APDS9960 breakout
+  ----> http://www.adafruit.com/products/3595
+
+  These sensors use I2C to communicate. The device's I2C address is 0x39
+
+  Adafruit invests time and resources providing this open source code,
+  please support Adafruit andopen-source hardware by purchasing products
+  from Adafruit!
+
+  Written by Dean Miller for Adafruit Industries.
+  BSD license, all text above must be included in any redistribution
+ ***************************************************************************/
+
+#include "Adafruit_APDS9960.h"
+
+//create the APDS9960 object
+Adafruit_APDS9960 apds;
+
+void setup() {
+  Serial.begin(115200);
+
+  if(!apds.begin()){
+    Serial.println("failed to initialize device! Please check your wiring.");
+  }
+  else Serial.println("Device initialized!");
+
+  //enable proximity mode
+  apds.enableProximity(true);
+
+}
+
+void loop() {
+
+  //print the proximity reading
+    Serial.println(apds.readProximity());
+ 
+}
